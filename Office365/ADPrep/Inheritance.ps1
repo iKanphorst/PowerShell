6d9159b3be0c812a51fd965badf743b377542bfd
iKanphorst <92365697+iKanphorst@users.noreply.github.com>
2022-02-24 20:55:19 +0000

    O365

---
+++ Office365/ADPrep/Inheritance.ps1
@@ -0,0 +1,20 @@
+$users = Get-ADUser -ldapfilter “(objectclass=user)” -searchbase “dc=pjmt,dc=local”
+ForEach($user in $users)
+{
+    # Binding the users to DS
+    $ou = [ADSI](“LDAP://” + $user)
+    $sec = $ou.psbase.objectSecurity
+ 
+    if ($sec.get_AreAccessRulesProtected())
+    {
+        $isProtected = $false ## allows inheritance
+        $preserveInheritance = $true ## preserver inherited rules
+        $sec.SetAccessRuleProtection($isProtected, $preserveInheritance)
+        $ou.psbase.commitchanges()
+        Write-Host “$user is now inherting permissions”;
+    }
+    else
+    {
+        Write-Host “$User Inheritable Permission already set”
+    }
+}
