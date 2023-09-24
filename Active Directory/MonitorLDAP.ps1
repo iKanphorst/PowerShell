7cf7d94b899f126b0b55571cb58383ee26f81757
Igor Lysenko <igorlkd@kanphorst.onmicrosoft.com>
2023-09-24 17:49:05 +0000

    Added option to save a log file

--- Active Directory/MonitorLDAP.ps1
+++ Active Directory/MonitorLDAP.ps1
@@ -1,5 +1,6 @@
 # Define the LDAP event ID (2887) for unencrypted LDAP traffic
 $ldapEventId = 2887
+$logFilePath = "C:\Path\To\Log\ldap_unencrypted.log"  # Specify the path for the log file
 
 # Define a function to handle LDAP event log entries
 function Handle-LdapEvent($event) {
@@ -7,13 +8,24 @@ function Handle-LdapEvent($event) {
     $sourceIp = [System.Net.IPAddress]::Parse($event.Properties[0].Value)
     $destinationIp = [System.Net.IPAddress]::Parse($event.Properties[1].Value)
 
+    # Log the event to a file
+    $logEntry = @"
+Unencrypted LDAP Traffic Detected!
+Source IP: $sourceIp
+Destination IP: $destinationIp
+Event Message: $message
+------------------------
+"@
+    
+    $logEntry | Out-File -FilePath $logFilePath -Append -Encoding UTF8
+
     Write-Host "Unencrypted LDAP Traffic Detected!"
     Write-Host "Source IP: $sourceIp"
     Write-Host "Destination IP: $destinationIp"
     Write-Host "Event Message: $message"
     Write-Host "------------------------"
     
-    # You can add your own actions here, such as sending an email or logging to a file.
+    # You can add additional actions here, such as sending an email notification.
 }
 
 # Create an event log subscription to monitor for LDAP events
